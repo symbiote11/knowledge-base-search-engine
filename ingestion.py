bb22ea7b705d885d325515c202caf0598a9a9728
symbiote11 <152368290+symbiote11@users.noreply.github.com>
2025-12-25 19:51:29 +0530

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -4,5 +4,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 def load_and_split_docs(file_path: str):
     loader = PyPDFLoader(file_path)
     documents = loader.load()
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
     return splitter.split_documents(documents)
