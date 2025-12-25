72025538d600ad0f6e1c98e18fe7f5c546c37698
symbiote11 <152368290+symbiote11@users.noreply.github.com>
2025-12-25 19:53:46 +0530

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,6 +1,10 @@
 from langchain_community.vectorstores import FAISS
-from app.config import embeddings
+from config import embeddings
 
 def load_retriever(index_path: str):
-    vectorstore = FAISS.load_local(index_path, embeddings, allow_dangerous_deserialization=True)
+    vectorstore = FAISS.load_local(
+        index_path,
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
     return vectorstore.as_retriever(search_kwargs={"k": 3})
