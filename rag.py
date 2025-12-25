5a2452cd2e71c2752de64e93d90c28723fa7446b
symbiote11 <152368290+symbiote11@users.noreply.github.com>
2025-12-25 19:54:40 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,7 +1,12 @@
 from langchain.chains import RetrievalQA
-from app.config import llm
-from app.retriever import load_retriever
+from config import llm
+from retriever import load_retriever
 
 def get_rag_chain(index_path: str):
     retriever = load_retriever(index_path)
-    return RetrievalQA.from_chain_type(llm=llm, retriever=retriever, chain_type="stuff")
+
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=retriever,
+        chain_type="stuff"
+    )
